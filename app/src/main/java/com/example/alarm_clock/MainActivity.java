8602479f0da3aa4345cedf4b809ac9eb7a0170e8
Hazemorn <nikitmiheenko@gmail.com>
2023-04-04 22:19:20 +0300

    second

--- app/src/main/java/com/example/alarm_clock/MainActivity.java
+++ app/src/main/java/com/example/alarm_clock/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.alarm_clock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -36,9 +40,18 @@ public class MainActivity extends AppCompatActivity {
                 calendar.set(calendar.MINUTE, materialTimePicker.getMinute());
                 calendar.set(calendar.MINUTE, Calendar.HOUR_OF_DAY, materialTimePicker.getHour());
 
+                AlarmManager alarMManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+                AlarmManager.AlarmClockInfo alarmClockInfo = new AlarmManager.AlarmClockInfo(calendar.getTimeInMillis(),);
             });
 
             materialTimePicker.show(getSupportFragmentManager(),"tag_picker");
         });
+    }
+
+    private PendingIntent getAlarmInfoPendingIntent(){
+        Intent alarmInfoIntent = new Intent(this, MainActivity.class);
+        alarmInfoIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_NEW_TASK);
+        return PendingIntent.getActivity(this, 0, alarmInfoIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
